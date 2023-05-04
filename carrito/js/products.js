ae47f75ab591df00f6b96f12f10340bf2b358984
Lucas Laborde <lucaslaborde.trabajo@gmail.com>
2023-05-04 08:16:49 -0300

    primer commit

---
+++ carrito/js/products.js
@@ -0,0 +1,62 @@
+const productos=[
+
+    {   id:1,
+        nombre:"Rtx 4070 ASUS", 
+        precio:50,
+        img: "https://drive.google.com/uc?export=download&id=1IAQVooRaGhsctl6oC7rwW7N-4jJvCVLf",
+        cantidad: 1,
+
+    
+    },
+
+    {
+        id:2,
+        nombre:"Corsair 750W",
+         precio:50, 
+         img: "https://drive.google.com/uc?export=download&id=18lP_lHHrZqLsbuEMKfe6vxdhokmzI6oe",
+         cantidad: 1,
+    
+    },
+
+    { id:3,
+        nombre:"Gabinete Gigabyte", 
+        precio:50, 
+        img: "https://drive.google.com/uc?export=download&id=1URCux1bRQOu4gbB4b5HFcNDc9EQsp6Qh",
+        cantidad: 1,
+    
+    },
+
+    {id:4,
+        nombre:"Combo Actualizacion",
+         precio:50,
+         img: "https://drive.google.com/uc?export=download&id=1nRtsaBXVk_qT3g2AQThsYWr2kfJAhcet",
+         cantidad: 1,
+         },
+
+    {
+        id:5,
+        nombre:"Solido 1TB Gigabyte", 
+        precio:50 ,
+        img:"https://drive.google.com/uc?export=download&id=1cObnFfCGygblj8ZzgDmRwAgs3uXhSDzh",
+        cantidad: 1,
+    }, 
+
+
+     {
+        id:6,
+        nombre: "Memoria ram 8GB",
+        precio: 50,
+        img : "https://drive.google.com/uc?export=download&id=1F7Qu9k4OBsoM5x0zzF0f2d5-numDMUDx",
+        cantidad : 1,
+
+     },
+
+
+     {
+        id:7,
+        nombre: "Disco Mecanico",
+        precio: 50,
+        img:"https://drive.google.com/uc?export=download&id=1dfPENJPvFQebeO5wHjn27LjhLKxzfGoC",
+        cantidad: 1,
+     },
+];
